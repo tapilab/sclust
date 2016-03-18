do correct idf computation

sclust/sclust.py
@@ -17,8 +17,8 @@ import re
 import sys
 
 
-def norm(tokens, doc_freqs):
-    return sqrt(np.sum((count / log10(1+doc_freqs[token]))**2
+def norm(tokens, doc_freqs, docnum):
+    return sqrt(np.sum(count * idf(token, doc_freqs, docnum)**2
                        for token, count in tokens.items()))
 
 
@@ -26,40 +26,42 @@ def tokenize(line):
     return re.findall('\w+', line.lower())
 
 
+def idf(token, doc_freqs, docnum):
+    return log10((docnum + 1) / doc_freqs[token])
+
 class Cluster:
     def __init__(self, token_counts):
         self.token_counts = Counter(token_counts)
-        self.size = 1
 
     def add(self, tokens):
         self.token_counts.update(tokens)
-        self.size += 1
-        # self.token_counts = tokens
 
-    def score(self, tokens, doc_norm, doc_freqs):
-        cluster_norm = norm(self.token_counts, doc_freqs)
+    def score(self, tokens, doc_norm, doc_freqs, docnum):
+        cluster_norm = norm(self.token_counts, doc_freqs, docnum)
         numer = 0
         for token, count in tokens.items():
-            numer += self.token_counts[token] * count / log10(1+doc_freqs[token])**2
+            numer += self.token_counts[token] * count * idf(token, doc_freqs, docnum)**2
         return numer / (cluster_norm * doc_norm)
 
 
 def run(batch_size, threshold):
     doc_freqs = Counter()
     clusters = []
+    docnum = 0
     for line in sys.stdin:
+        docnum += 1
         line = line.strip()
         tokens = Counter(tokenize(line))
         doc_freqs.update(tokens)
-        this_norm = norm(tokens, doc_freqs)
+        this_norm = norm(tokens, doc_freqs, docnum)
         # What is the word with highest tfidf weight? Used to filter comparisons.
         top_words = sorted((doc_freqs[token], token) for token in tokens if doc_freqs[token] > 1)
-        top_words = set(w[1] for w in top_words[:2])
+        top_words = set(w[1] for w in top_words[:3])
         best_cluster = -1
         best_score = -1
         for ci, cluster in enumerate(clusters):
             if len(top_words) == 0 or len(top_words & set(cluster.token_counts)) > 0:
-                score = cluster.score(tokens, this_norm, doc_freqs)
+                score = cluster.score(tokens, this_norm, doc_freqs, docnum)
                 if score > best_score and score > threshold:
                     best_cluster = ci
                     best_score = score