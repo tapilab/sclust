271af54f3a55fcee6b71ffa7c5cf0763487c6f98
Aron Culotta <aronwc@gmail.com>
2016-03-18 11:30:53 -0500

    print at end

--- sclust/sclust_summarize.py
+++ sclust/sclust_summarize.py
@@ -17,6 +17,11 @@ import numpy as np
 import re
 import sys
 
+def print_summary(cluster_counts, clusterid2line, num_to_print, lineno):
+    print('\n---------%d lines---------\n' % lineno)
+    for clusterid, count in cluster_counts.most_common(num_to_print):
+        print('%d\t%s' % (count, clusterid2line[clusterid]))
+    sys.stdout.flush()
 
 def run(frequency, num_to_print):
     cluster_counts = Counter()
@@ -29,10 +34,8 @@ def run(frequency, num_to_print):
         cluster_counts[fields[0]] += 1
         clusterid2line[fields[0]] = line
         if lineno % frequency == 0:
-            print('\n---------%d lines---------\n' % lineno)
-            for clusterid, count in cluster_counts.most_common(num_to_print):
-                print('%d\t%s' % (count, clusterid2line[clusterid]))
-            sys.stdout.flush()
+            print_summary(cluster_counts, clusterid2line, num_to_print, lineno)
+    print_summary(cluster_counts, clusterid2line, num_to_print, lineno)
 
 
 # Weirdness when piping to unix tools. See http://stackoverflow.com/a/26736013/1756896
